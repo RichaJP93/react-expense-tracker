1f124fb25ccebe629a237261fe21485c0b79a905
Joseph Richardson <richardsonjp93@gmail.com>
2020-08-04 14:46:20 +0100

    set up connection to mongo db

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
@@ -13,4 +16,4 @@ app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold));
